863f218ee982aa042b3b37dce292511204ac2fb7
Prashanthtella <prasanthkumartella@gmail.com>
2021-05-17 05:23:39 -0700

    webapp

---
+++ src/main/scala/webapp/routes/BabelCamelRoute.scala
@@ -0,0 +1,9 @@
+package webapp.routes
+
+import io.xtech.babel.camel.builder.RouteBuilder
+
+class BabelCamelRoute extends RouteBuilder {
+
+  from("direct:input").routeId("route1").processBody("hello " + _).to("direct:output")
+}
+
